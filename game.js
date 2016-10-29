ebadd88f0dfe8e93f46054bbce3b82ac6f417524
Elizabeth <wolensky@work.co>
2016-10-29 19:59:30 -0400

    square now moves around the canvas

--- game.js
+++ game.js
@@ -1,7 +1,4 @@
 (() => {
-  console.log("Hello BEST WOMAN IN THE WHOLE WORLD!");
-  console.log("(who is Liz)");
-
   class Location {
     constructor(lane, col) {
       this.lane = lane;
@@ -15,7 +12,24 @@
       if (this.col !== location.col) {
         return false;
       }
+
       return true;
+    }
+
+    moveRight() {
+      this.col++;
+    }
+
+    moveLeft() {
+      this.col--;
+    }
+
+    moveUp() {
+      this.lane--;
+    }
+
+    moveDown() {
+      this.lane++;
     }
   }
 
@@ -23,25 +37,38 @@
     constructor() {
       this.laneCount = 4;
       this.colCount = 8;
-      this.bikeLocation = new Location(2,7);
+      this.bikeLocation = new Location(0, 0);
       this.canvas = document.getElementById("gameGrid");
       this.ctx = this.canvas.getContext("2d");
     }
 
-    // setBikeLocation(lane, col) {
-      // this.bikeLocation = new Location(lane, col);
-    // }
+    moveBikeRight() {
+      this.bikeLocation.moveRight();
+    }
+
+    moveBikeLeft() {
+      this.bikeLocation.moveLeft();
+    }
+
+    moveBikeUp() {
+      this.bikeLocation.moveUp();
+    }
+
+    moveBikeDown() {
+      this.bikeLocation.moveDown();
+    }
 
     drawGrid() {
       for (var i = 0; i < this.laneCount; i++) {
         for (var j = 0; j < this.colCount; j++) {
-          this.drawCell(i,j);
+          this.drawCell(i, j);
         }
       }
     }
 
     drawCell(lane, col) {
-      let loc = new Location(lane, col);
+      const loc = new Location(lane, col);
+
       if (loc.equalTo(this.bikeLocation)) {
         this.ctx.fillStyle = "#52144c";
         this.ctx.fillRect(100 * col, 100 * lane, 100, 100);
@@ -52,6 +79,24 @@
     }
   }
 
-  let game = new GameGrid();
+  let handleKeydown = (e, g) => {
+    if(e.key === 'ArrowRight') {
+      g.moveBikeRight();
+    } else if (e.key === 'ArrowLeft') {
+      g.moveBikeLeft();
+    } else if (e.key === 'ArrowUp') {
+      g.moveBikeUp();
+    } else if (e.key === 'ArrowDown') {
+      g.moveBikeDown();
+    } else {
+      console.log('you suck');
+    }
+    
+    g.drawGrid();
+  }
+  const game = new GameGrid();
+  
+  document.addEventListener('keydown', (e) => handleKeydown(e, game));
+
   game.drawGrid();
 })();
